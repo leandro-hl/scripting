02f47b6f862f1266d5ea8b5d5bb5b802f1fe8e7b
Leandro Herenu <lherenu@coalfire.com>
2021-05-11 23:46:54 -0300

    file type v1.1

--- file-type.sh
+++ file-type.sh
@@ -7,6 +7,7 @@ DIRECTORIO=0
 RECURSIVO=0
 ORDENADO=0
 
+
 usage() {
 cat << EOF
 Version: V1
@@ -18,7 +19,7 @@ EOF
 }
 
 while getopts "drtv" o; do
-    case "${o}" in
+    case "${o}" in                
         d)
             #   Directorio. Informar para todos los hijos
             DIRECTORIO=1
@@ -45,12 +46,26 @@ while getopts "drtv" o; do
 done
 
 for last; do true; done
-ARCHIVO=$last
+LOOKUP=$last
 
-echo "    TIPO DE ARCHIVO    USR DUEÑO    GRP DUEÑO    TAMAÑO    LINKS"
-
-if [ "$DIRECTORIO" -eq 1 ]; then
-    ls -l $ARCHIVO | awk '$0="    "$3"    "$4"    "'
+# si es directorio buscamos en todos sus hijos
+if [[ "$DIRECTORIO" -eq 1  ]]; then
+    echo "Nombre directorio: $LOOKUP";
+    grilla="$archivo $EXTENSION $PERMISOS"
+else
+    echo "Nombre: $LOOKUP"
+    grilla="$EXTENSION $PERMISOS"
 fi
+
+[[ "$RECURSIVO" -eq 1  ]] && params+=(-R)
+[[ "$ORDENADO" -eq 1  ]] && params+=(--sort)
+
+for archivo in `ls -1 "${params[@]}" $LOOKUP`; do
+    EXTENSION= file $archivo | awk '{$1=""; printf "%s%s", substr($0,1,10), "\t"}'
+    PERMISOS= ls -l $archivo | awk '{for(x=3; x<= 5; x++){ printf "%s%s", substr($x,1,10), "\t"}}'
+
+    echo $grilla
+done
+
 
 echo "DIR ${DIRECTORIO} REC ${RECURSIVO} ORD ${ORDENADO} ${ARCHIVO}"
